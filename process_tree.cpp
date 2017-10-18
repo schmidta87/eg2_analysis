4e4590e8550490e4c16a2c8dc1afa4f40c4d889f
Axel Schmidt <schmidta@mit.edu>
2017-10-18 18:56:01 -0400

    Modified process tree to allow multiple files.

--- process_tree.cpp
+++ process_tree.cpp
@@ -7,30 +7,20 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  if (argc != 5)
+  if (argc < 5)
     {
       cerr << "Wrong number of arguments! Instead use\n"
-	   << "\t/path/to/tree/file /path/to/output/text/file [# burn-in] [# to keep one of] \n\n";
+	   << "\t[# burn-in] [# to keep one of] /path/to/output/file List: /path/to/first/input /path/to/second/input ...\n\n";
       return -1;
     }
 
   // Get the parameters
-  int burn_in = atoi(argv[3]);
-  int keep_one = atoi(argv[4]);
-
-  // Read in the file
-  TFile * infile = new TFile(argv[1]);
-  TTree * intree = (TTree*) infile->Get("mcmc");
-  double a1, a2, b1, b2, sigPerp, logP;
-  intree->SetBranchAddress("a1",&a1);
-  intree->SetBranchAddress("a2",&a2);
-  intree->SetBranchAddress("b1",&b1);
-  intree->SetBranchAddress("b2",&b2);
-  intree->SetBranchAddress("sigPerp",&sigPerp);
-  intree->SetBranchAddress("logposterior",&logP);
+  int burn_in = atoi(argv[1]);
+  int keep_one = atoi(argv[2]);
 
   // Create the output file
-  TFile * outfile = new TFile(argv[2],"RECREATE");
+  double a1, a2, b1, b2, sigPerp, logP;
+  TFile * outfile = new TFile(argv[3],"RECREATE");
   TTree * outtree = new TTree("mcmc","Processed mcmc results");
   outtree->Branch("a1",&a1,"a1/D");
   outtree->Branch("a2",&a2,"a2/D");
@@ -39,22 +29,37 @@ int main(int argc, char **argv)
   outtree->Branch("sigPerp",&sigPerp,"sigPerp/D");
   outtree->Branch("logposterior",&logP,"logP/D");
 
-  // Loop over the tree
-  for (int i=0 ; i<intree->GetEntries() ; i++)
+  // Loop over the input files
+  for (int j=4 ; j<argc ; j++)
     {
-      intree->GetEvent(i);
+      // Read in the file
+      TFile * infile = new TFile(argv[j]);
+      TTree * intree = (TTree*) infile->Get("mcmc");
+      intree->SetBranchAddress("a1",&a1);
+      intree->SetBranchAddress("a2",&a2);
+      intree->SetBranchAddress("b1",&b1);
+      intree->SetBranchAddress("b2",&b2);
+      intree->SetBranchAddress("sigPerp",&sigPerp);
+      intree->SetBranchAddress("logposterior",&logP);
 
-      if (i<burn_in)
-	continue;
+      // Loop over the tree
+      for (int i=0 ; i<intree->GetEntries() ; i++)
+	{
+	  intree->GetEvent(i);
+	  
+	  if (i<burn_in)
+	    continue;
+	  
+	  if (i%keep_one ==0)
+	    outtree->Fill();
+	}
 
-      if (i%keep_one ==0)
-	outtree->Fill();
+      infile->Close();
     }
-
+      
   outfile->cd();
   outtree->Write();
   outfile->Close();
-  infile->Close();
 
   return 0;
 }
