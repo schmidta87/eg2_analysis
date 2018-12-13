d205f5fd0f32c5823760b065e3cf1cac5e7be069
Jackson Pybus <Jrpybus@mit.edu>
2018-12-13 12:36:49 -0800

    Temp pp-uo-np ratio modifications.

--- generator/pp_np_cut.cpp
+++ generator/pp_np_cut.cpp
@@ -6,10 +6,12 @@
 #include "TTree.h"
 #include "TH1D.h"
 #include "TGraphAsymmErrors.h"
+#include "TRandom3.h"
 
 #include "AccMap.h"
 #include "fiducials.h"
 #include "Nuclear_Info.h"
+#include "Cross_Sections.h"
 
 using namespace std;
 
@@ -40,10 +42,41 @@ int main(int argc, char ** argv)
 
   // Output Tree
   TTree * T = new TTree("T","Cut Tree");
+  Float_t Q2, Xb, Pe[3], Pe_size, theta_e, phi_e, Pp[2][3], Pp_size[2], pq_angle[2], Ep[2], theta_p[2], phi_p[2], nu, q[3];
+  Float_t Pmiss_q_angle[2], Pmiss_size[2], Pmiss[2][3];
+  Float_t z = 0.;
+  Float_t Rp[2][3]={{0.,0.,-22.25},{0.,0.,-22.25}};
   Double_t weight;
   Double_t pRec_Mag;
+  Double_t pLead_Mag;
+  Int_t nmb = 2;
+  
+  T->Branch("Q2",&Q2,"Q2/F");
+  T->Branch("Xb",&Xb,"Xb/F");
+  T->Branch("nu",&nu,"nu/F");
+  T->Branch("q",q,"q[3]/F");
+  T->Branch("Pe",Pe,"Pe[3]/F");
+  T->Branch("Pe_size",&Pe_size,"Pe_size/F");
+  T->Branch("theta_e",&theta_e,"theta_e/F");
+  T->Branch("phi_e",&phi_e,"phi_e/F");
+  T->Branch("Pp",Pp,"Pp[2][3]/F");
+  T->Branch("Pp_size",Pp_size,"Pp_size[2]/F");
+  T->Branch("pq_angle",pq_angle,"pq_angle[2]/F");
+  T->Branch("Ep",Ep,"Ep[2]/F");
+  T->Branch("theta_p",theta_p,"theta_p[2]/F");
+  T->Branch("phi_p",phi_p,"phi_p[2]/F");
+  T->Branch("Pmiss_q_angle",Pmiss_q_angle,"Pmiss_q_angle[2]/F");
+  T->Branch("Pmiss_size",Pmiss_size,"Pmiss_size[2]/F");
+  T->Branch("Pmiss",Pmiss,"Pmiss[2][3]/F");
+  T->Branch("Rp",Rp,"Rp[2][3]/F");
   T->Branch("weight",&weight,"weight/D");
   T->Branch("pRec",&pRec_Mag,"pRec/D");
+  T->Branch("pLead",&pLead_Mag,"pLead/D");
+
+  TRandom3 myRand(0);
+  double a = 0.03;
+  double b = 0.06;
+  Cross_Sections myCS;
   
   // Loop over all events
   const int nEvents = inTree->GetEntries(); // this is a key number for the weight
@@ -58,22 +91,40 @@ int main(int argc, char ** argv)
       if (rec_type != pCode)
 	continue;
 
-      // Apply weight for detecting e, p      
-      // weight = gen_weight * eMap.accept(ve) * pMap.accept(vlead) * 1.E33; // put it in nb to make it macroscopic
-
-      weight = gen_weight;
-      if (weight <= 0.)
+      if ((lead_type == pCode) == (num_p == 1))
 	continue;
-
+      
       // Create vectors for the particles
+      TVector3 vrec(gen_pRec[0],gen_pRec[1],gen_pRec[2]);
       TVector3 ve(gen_pe[0],gen_pe[1],gen_pe[2]);
       TVector3 vq = TVector3(0.,0.,eg2beam) - ve;
       TVector3 vlead(gen_pLead[0],gen_pLead[1],gen_pLead[2]);
-      TVector3 vrec(gen_pRec[0],gen_pRec[1],gen_pRec[2]);
+      
+      // Fiducial cuts
+      if (!accept_electron(ve))
+	continue;
+      if (!accept_proton(vlead))
+	continue;
+      if (!accept_proton(vrec))
+	continue;
+      if (!accept_neutron(vlead))
+	continue;
+
+      // Apply Smearing
+      vlead += vlead*myRand.Gaus(0,a+b*vlead.Mag());
+      
+      // Apply weight for cross sections
+      if (lead_type == pCode)
+	weight = gen_weight/(2*myCS.sigmaCC1(eg2beam,ve,vlead,true));
+      else
+	weight = gen_weight/(myCS.sigmaCC1(eg2beam,ve,vlead,false));
+      if (weight <= 0.)
+	continue;
+      
       TVector3 vmiss=vlead-vq;
       TVector3 vcm=vmiss+vrec;
       TVector3 vrel=0.5*(vmiss-vrec);
-
+      
       double gen_pMiss_Mag = vmiss.Mag();
       double gen_pe_Mag = ve.Mag();
       double gen_QSq = 2. * eg2beam * gen_pe_Mag * (1. - ve.CosTheta());
@@ -103,21 +154,45 @@ int main(int argc, char ** argv)
       if (gen_pRec_Mag < 0.35)
 	continue;
 
-      // Fiducial cuts
-      if (!accept_electron(ve))
-	continue;
-      if (!accept_proton(vlead))
-	continue;
-      if (!accept_proton(vrec))
-	continue;
-      if (!accept_neutron(vlead))
-	continue;
-
       // Load up tree
       pRec_Mag = gen_pRec_Mag;
+      pLead_Mag = gen_pLead_Mag;
+
+      // Load up tree
+      Q2 = gen_QSq;
+      Xb = gen_xB;
+      nu = gen_nu;
+      Pe_size = gen_pe_Mag;
+      theta_e = ve.Theta() * 180./M_PI;
+      phi_e = ve.Phi()*180./M_PI;
+      if (phi_e < -30.) phi_e += 360;
+      Pp_size[0] = gen_pLead_Mag;
+      Pp_size[1] = gen_pRec_Mag;
+      pq_angle[0] = vq.Angle(vlead)*180./M_PI;
+      pq_angle[1] = vq.Angle(vrec)*180./M_PI;
+      Ep[0] = sqrt(gen_pLead_Mag*gen_pLead_Mag + mN*mN);
+      Ep[1] = sqrt(gen_pRec_Mag*gen_pRec_Mag + mN*mN);
+      theta_p[0] = vlead.Theta()*180./M_PI;
+      theta_p[0] = vrec.Theta()*180./M_PI;
+      phi_p[0] = vlead.Phi()*180./M_PI;
+      if (phi_p[0] < -30.) phi_p[0] += 360;
+      phi_p[1] = vrec.Phi()*180./M_PI;
+      if (phi_p[1] < -30.) phi_p[1] += 360;
+      for (int i=0 ; i<3 ; i++)
+	{
+	  q[i] = vq[i];
+	  Pe[i] = gen_pe[i];
+	  Pp[0][i] = vlead[i];
+	  Pp[1][i] = vrec[i];
+	  Pmiss[0][i] = vlead[i] - vq[i];
+	  Pmiss[1][i] = vrec[i] - vq[i];
+	}
+
+      Pmiss_q_angle[0] = (vlead - vq).Angle(vq) * 180./M_PI;
+      Pmiss_q_angle[1] = (vrec - vq).Angle(vq) * 180./M_PI;
       
-      if ((lead_type == pCode) == (num_p == 1))
-	continue;
+      Pmiss_size[0] = (vlead-vq).Mag();
+      Pmiss_size[1] = (vrec - vq).Mag();
       
       T->Fill();
     }
--- generator/pp_np_prec.cpp
+++ generator/pp_np_prec.cpp
@@ -27,27 +27,49 @@ int main(int argc, char ** argv)
 	TFile * fnp = new TFile(argv[2]);
 	TFile * fo = new TFile(argv[3],"RECREATE");
 
-	// Create histograms
-        
+	// Create histograms        
 	TH1D * hpp_pRec = new TH1D("epp_Pr","epp;pRec [GeV];Counts",26,0.35,1.0);
 	hpp_pRec->Sumw2();
 	TH1D * hnp_pRec = new TH1D("enp_Pr","enp;pRec [GeV];Counts",26,0.35,1.0);
 	hnp_pRec->Sumw2();
+
+	Double_t bins[3] = {0.35,0.6,1.0};
+	TH1D * hpp_pRec_2 = new TH1D("epp_Pr_2","epp;pRec [GeV];Counts",2,bins);
+	hpp_pRec_2->Sumw2();
+	TH1D * hnp_pRec_2 = new TH1D("enp_Pr_2","enp;pRec [GeV];Counts",2,bins);
+	hnp_pRec_2->Sumw2();
+
+	TH1D * hpp_pMiss = new TH1D("epp_Pmiss","epp;pMiss [GeV];Counts",26,0.35,1.0);
+	hpp_pMiss->Sumw2();
+	TH1D * hnp_pMiss = new TH1D("enp_Pmiss","enp;pMiss [GeV];Counts",26,0.35,1.0);
+	hnp_pMiss->Sumw2();
+
 	
 	// Loop over pp tree
 	cerr << " Looping over pp tree...\n";
 	TTree * tpp = (TTree*)fpp->Get("T");
 	Double_t weight;
 	Double_t pRec;
-        
+	Double_t pLead;
+	Float_t Pmiss[2][3];
+	Double_t pMiss;
+	
 	tpp->SetBranchAddress("weight",&weight);
 	tpp->SetBranchAddress("pRec",&pRec);
-        
+	tpp->SetBranchAddress("pLead",&pLead);
+	tpp->SetBranchAddress("Pmiss",&Pmiss);
+	
 	for (int event =0 ; event < tpp->GetEntries() ; event++)
-	{
+	  {
+	  
 		tpp->GetEvent(event);
 
+		pMiss = sqrt(Pmiss[0][0]*Pmiss[0][0]+Pmiss[0][1]*Pmiss[0][1]+Pmiss[0][2]*Pmiss[0][2]);
+
 	        hpp_pRec->Fill(pRec,weight);
+	        hpp_pRec_2->Fill(pRec,weight);
+
+		hpp_pMiss->Fill(pMiss,weight);
 	}
 
 	// Loop over np tree
@@ -56,31 +78,60 @@ int main(int argc, char ** argv)
 
 	tnp->SetBranchAddress("weight",&weight);
 	tnp->SetBranchAddress("pRec",&pRec);
+	tnp->SetBranchAddress("pLead",&pLead);
+	tnp->SetBranchAddress("Pmiss",&Pmiss);
         
       	for (int event =0 ; event < tnp->GetEntries() ; event++)
 	{
 		tnp->GetEvent(event);
 
+		pMiss = sqrt(Pmiss[0][0]*Pmiss[0][0]+Pmiss[0][1]*Pmiss[0][1]+Pmiss[0][2]*Pmiss[0][2]);
+
 	        hnp_pRec->Fill(pRec,weight);
+	        hnp_pRec_2->Fill(pRec,weight);
+
+		hnp_pMiss->Fill(pMiss,weight);
 	}
 	
 	fpp->Close();
 	fnp->Close();
 
 	// pp2np hist
-	//TRatioPlot * pp_to_np = new TRatioPlot(hpp_pRec,hnp_pRec);
-
 	TH1F *pp_to_np = (TH1F*)hpp_pRec->Clone("pp_to_np");
 	pp_to_np->SetName("pp_to_np");
 	pp_to_np->SetTitle("pp_to_np;p_rec [GeV];pp_to_np ratio");
 	pp_to_np->Divide(hnp_pRec);
-	pp_to_np->Scale(0.2);
 	
+	TH1F *pp_to_np_2 = (TH1F*)hpp_pRec_2->Clone("pp_to_np_2");
+	pp_to_np_2->SetName("pp_to_np_2");
+	pp_to_np_2->SetTitle("pp_to_np_2;p_rec [GeV];pp_to_np ratio");
+	pp_to_np_2->Divide(hnp_pRec_2);	
+
+	TH1F *pp_to_np_miss = (TH1F*)hpp_pMiss->Clone("pp_to_np_miss");
+	pp_to_np_miss->SetName("pp_to_np_miss");
+	pp_to_np_miss->SetTitle("pp_to_np_miss;p_miss [GeV];pp_to_np ratio");
+	pp_to_np_miss->Divide(hnp_pMiss);
+	
+	pp_to_np->SetMaximum(0.4);
+	pp_to_np->SetMinimum(0.0);
+	pp_to_np_2->SetMaximum(0.2);
+	pp_to_np_2->SetMinimum(0.0);
+	hpp_pRec->SetMinimum(0.0);
+	hnp_pRec->SetMinimum(0.0);
+	hpp_pRec_2->SetMinimum(0.0);
+	hnp_pRec_2->SetMinimum(0.0);
+        
 	// Write out
 	fo->cd();
 	pp_to_np->Write();
 	hpp_pRec->Write();
 	hnp_pRec->Write();
+	pp_to_np_2->Write();
+	hpp_pRec_2->Write();
+	hnp_pRec_2->Write();
+	pp_to_np_miss->Write();
+	hpp_pMiss->Write();
+	hnp_pMiss->Write();
 	fo->Close();
 
 	return 0;
