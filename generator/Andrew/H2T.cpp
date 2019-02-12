5d4ea150ffc2ec073ff12e8a44e24d3dbffb65cb
Andrew <awild@mit.edu>
2019-02-12 10:16:07 -0500

    corrections to H2T

--- generator/Andrew/H2T.cpp
+++ generator/Andrew/H2T.cpp
@@ -45,14 +45,15 @@ int main(int argc, char ** argv){
       if(isTH1D){
 	TH1D * WHist = (TH1D*)Data->Get(List->At(i)->GetName());	  
 	string a = List->At(i)->GetName();
-	  if(j<0){
-	    file <<"[Column "<<k<<": "<<a<<" xAxis] [Column "<<(k+1)<<": "<<a<<" yAxis] [Column "<<(k+2)<<": "<<a<<" ySTD] \t TH1D"<<endl;
-	    k=k+3;
-	  }
-	  else if(j < WHist->GetXaxis()->GetNbins()){
-	    file << WHist->GetBinCenter(j) << " " << WHist->GetBinContent(j) << " " << WHist->GetBinError(j) << " ";
-	  }
-	  else file <<"N N N ";
+	int N = WHist->GetXaxis()->GetNbins();
+	if(j<0){
+	  file <<"[Column "<<k<<": "<<a<<" xAxis] [Column "<<(k+1)<<": "<<a<<" yAxis] [Column "<<(k+2)<<": "<<a<<" ySTD] \t TH1D"<<endl;
+	  k=k+3;
+	}
+	else if((j>=0)&&(j<N)){
+	  file << WHist->GetBinCenter(j) << " " << WHist->GetBinContent(j) << " " << WHist->GetBinError(j) << " ";
+	}
+	else file <<"N N N ";
       }
       
     
@@ -60,11 +61,12 @@ int main(int argc, char ** argv){
       else if(isTH2D){
 	TH2D * SHist = (TH2D*)Data->Get(List->At(i)->GetName());
 	string a = List->At(i)->GetName();
+	int N = SHist->GetXaxis()->GetNbins();
 	if(j<0){
 	  file <<"[Column "<<k<<": "<<a<<" xAxis] [Column "<<(k+1)<<": "<<a<<" yAxis] [Column "<<(k+2)<<": "<<a<<" ySTD] [Column "<<(k+3)<<": "<<a<<" yError] \t TH2D"<<endl;
 	  k=k+4;      
 	}
-	else if(j < SHist->GetXaxis()->GetNbins()){
+	else if((j>=0)&&(j<N)){
 	  TH1D * HistProj = SHist->ProjectionY("epsilonProj",j,(j+1));
 	  double n = HistProj->GetEntries();
 	  double std = HistProj->GetStdDev();
@@ -77,12 +79,13 @@ int main(int argc, char ** argv){
       else if(isTGAE){
 	TGraphAsymmErrors * myTGAE = (TGraphAsymmErrors*)Data->Get(List->At(i)->GetName());
 	string a = List->At(i)->GetName();
+	int N =myTGAE->GetN();
 	if(j<0){
 	  file <<"[Column "<<k<<": "<<a<<" xAxis] [Column "<<(k+1)<<": "<<a<<" xErrorLow] [Column "<<(k+2)<<": "<<a<<" xErrorHigh]"
 	       <<"[Column "<<(k+3)<<": "<<a<<" yAxis] [Column "<<(k+4)<<": "<<a<<" yErrorLow] [Column "<<(k+5)<<": "<<a<<" yErrorHigh] \t TGAE"<<endl;
 	  k=k+6;      
 	}
-	if(j < myTGAE->GetN()){
+	if((j>=0)&&(j<N)){
 	  Double_t x,y;
 	  myTGAE->GetPoint(j,x,y);
 	  file << x << " " << myTGAE->GetErrorXlow(j) << " " << myTGAE->GetErrorXhigh(j) << " "
