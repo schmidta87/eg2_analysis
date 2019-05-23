7cc3a0b51e9a80b1ca5b4ed648f9bd4cc0199ec5
jrpybus <jrpybus@eofe5.eth.cluster>
2019-05-23 10:27:50 -0400

    Corrected other nuclear masses, removing electron masses.

--- generator/Nuclear_Info.h
+++ generator/Nuclear_Info.h
@@ -6,15 +6,15 @@
 
  // nuclear masses
  const double m_1H = mN;
- const double m_2H = 2.01410178*mU;
- const double m_4He = 4.00260325415 * mU;
- const double m_6Li = 6.015122795 * mU;
- const double m_8Be = 8.00530510 * mU;
- const double m_10B = 10.0129370 * mU;
- const double m_11B = 11.0093054 * mU;
+ const double m_2H = 2.01410178*mU - me;
+ const double m_4He = 4.00260325415 * mU - 2*me;
+ const double m_6Li = 6.015122795 * mU - 3*me;
+ const double m_8Be = 8.00530510 * mU - 4*me;
+ const double m_10B = 10.0129370 * mU - 5*me;
+ const double m_11B = 11.0093054 * mU - 5*me;
  const double m_12C = 12.*mU - 6*me;
- const double m_14N = 14.0030740048*mU;
- const double m_16O = 15.99491461956*mU;
+ const double m_14N = 14.0030740048*mU - 7*me;
+ const double m_16O = 15.99491461956*mU - 8*me;
 
  // nucleon codes
  const int pCode=2212;
